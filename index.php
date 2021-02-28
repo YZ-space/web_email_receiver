34589c8ac0b4c83c87634ab7fce3bfa7e394c515
jakez <zhu.yifa@husky.neu.edu>
2021-02-28 22:44:07 +0800

    completed basic email receive function

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-index.php
+<html>
+<head>
+<title>Web E-mail Receiver</title>
+</head>
+
+<body>
+
+<form action="process.php" method="post">
+    <p>
+    Name:<br> <input type="name" name="name" id="name"><br>
+    </p>
+    <p>
+    Email:<br> <input type="email" name="email" id="email"><br>
+    </p>
+    <p>
+    Subject:<br> <input type="subject" name="subject" id="subject" cols="30" rows="1"><br>
+    </p>
+    <textarea name="comment" id="comment" cols="45" rows="5"></textarea><br>
+    <input type="submit" name="submit" id="submit" value="Send mail">
+</form>
+
+</body>
+</html>
--- process.php
+++ process.php
@@ -1,1 +1,20 @@
-process.php
+<?php
+$to = "email.com"; /* the receiver email*/
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$comment = $_POST['comment'];
+
+// The following are headers
+$headers .="Content-type: text/html:\r\n";
+$headers .="From: $email";
+
+// The following is the mail function
+mail($to, $subject, $comment, $headers);
+
+// The following is the message printing to the user
+echo "Email has been sent! Thank you $name";
+
+?>
